ae5ce8e2bc068333609a4b422add5296eaed4796
Yağız <yagizbas10@gmail.com>
2024-09-15 10:34:44 +0300

    b

--- Python/Python Dersler/15.09.2024.py
+++ Python/Python Dersler/15.09.2024.py
@@ -1,8 +1,25 @@
-def topla(*args):
-    toplam = 0
-    for i in args:
-        toplam += i
-    return toplam
-x= topla(1,2,3,4,5,6,7)
-print(x)
-print(type(x))
+# def topla(*args):
+#     toplam = 0
+#     for i in args:
+#         toplam += i
+#     return toplam
+# x= topla(1,2,3,4,5,6,7)
+# print(x)
+# print(type(x))
+
+# def yaz(*args):
+#     return args
+
+
+#Local Global değişkenleri
+
+def Vücütkitleindeks(kilo,boy):
+    sonuc = kilo / boy**2
+    sonuc2 = kilo/boy
+    return sonuc,sonuc2
+
+a = int(input("Boyunuzu giriniz: "))
+b = float(input("Kilonuzu giriniz: "))
+
+m = Vücütkitleindeks(a,b)
+print(m)
