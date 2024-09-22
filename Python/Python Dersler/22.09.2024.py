100c9bba35c8ab99ce3b72321ce2dc04e2289d73
Yağız <yagizbas10@gmail.com>
2024-09-22 13:33:29 +0300

    h

--- Python/Python Dersler/22.09.2024.py
+++ Python/Python Dersler/22.09.2024.py
@@ -43,31 +43,66 @@
 
 #########################   DECORATORS   #################################
 
-import time
-import math
-def zaman_hesapla(fonk):
-    def wrapper(*args,**kwargs):
-        başlangıç = time.time() #şuan ki saat
-        fonk(*args,**kwargs)
-        bitiş = time.time()
-        return f"İşlem {bitiş-başlangıç} saniyede tamamlanmıştır."
-    return wrapper
+# import time
+# import math
+# def zaman_hesapla(fonk):
+#     def wrapper(*args,**kwargs):
+#         başlangıç = time.time() #şuan ki saat
+#         fonk(*args,**kwargs)
+#         bitiş = time.time()
+#         return f"İşlem {bitiş-başlangıç} saniyede tamamlanmıştır."
+#     return wrapper
 
-@zaman_hesapla
-def üs_al(x,y):
-    time.sleep(1)
-    return pow(x,y)
+# @zaman_hesapla
+# def üs_al(x,y):
+#     time.sleep(1)
+#     return pow(x,y)
 
-@zaman_hesapla
-def topla(a,b):
-    return a+b
+# @zaman_hesapla
+# def topla(a,b):
+#     return a+b
 
-@zaman_hesapla
-def küp_al(d):
-    for i in d:
-        sonuç = i**3
-    return sonuç
+# @zaman_hesapla
+# def küp_al(d):
+#     for i in d:
+#         sonuç = i**3
+#     return sonuç
 
-print(üs_al(2,5))
-print(küp_al(range(1,21)))
-print(topla(5,2))
+# print(üs_al(2,5))
+# print(küp_al(range(1,21)))
+# print(topla(5,2))
+
+
+############ MODULLER ##############
+
+#Moduller ----> Paket
+#Paketler ----> Kütüphane
+
+import math as takma_isim
+# Takma isim verir
+from math import pow,sqrt
+# spesifik alır
+from math import*
+# math. demeden kullanabilirsin
+
+
+#**********************************************************************#
+
+# yüklü olan kütüphaneleri pip freeze ile görünteleyebiliriz
+# sanal ortam (virtual environement)
+# sanal ortam: python -m venv sanal_ortam_ismi
+# sanal ortam aktifleştirme: sanal_ortam_klasörü/scripts/activate
+# kütüphane kurulumu: pip install kütüphane_ismi -----> en son versiyon
+# kütüphane kurulumu: pip install kütüphane_ismi==4.2.12 (belirtilen versiyonu yükler)
+
+from pandas import*
+
+mydataset= {
+    "arabalar":["BMW","Volvo","Ford"],
+    "puan":[3,7,2]
+}
+
+m = DataFrame(mydataset)
+print(m)
+
+#Python yorumlayıcıyı sanal ortamda seçmek için: ctrl + p ---- >Select Interpreter
